d0835ae893f9775e0427e4d9c81795feb310dcf9
SylwesterS <sylancompany@gmail.com>
2019-09-09 18:54:05 +0100

    Started Evaluation

--- FootballJets/Assets/WinGameController.cs
+++ FootballJets/Assets/WinGameController.cs
@@ -33,8 +33,8 @@ public class WinGameController : MonoBehaviour
     }
     public void ShowWinScreen()
     {
-        // TODO
-        sceneSwitcher.SceneSwitch(2);
+       
+        sceneSwitcher.SceneSwitch(3);
     }
     private void SetStatus()
     {
